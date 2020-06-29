f1a1bdf603fd2189b57c333dbf9f8b14851efc7c
Martijn Goedhart <goedhart.martijn@gmail.com>
2020-06-29 22:24:46 +0200

    policy.pp: change priority to order to align with rest of code

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -4,22 +4,16 @@ define shorewall::policy (
     $source,
     $dest,
     $action,
-    $priority  = '50',
+    $order     = '50',
     $log_level = '-',
     $ipv4      = $::shorewall::ipv4,
     $ipv6      = $::shorewall::ipv6,
 ) {
-    validate_re($priority, ['^\d+$','last'], "Valid values for $priority are any integer or 'last'.")
-
-    if $priority == 'last' {
-        $order = 'q-last'
-    } else {
-        $order = "p-${priority}"
-    }
+    validate_re($order, ['^\d+$'], "Valid values for $order must be an integer.")
 
     if $ipv4 {
         concat::fragment { "policy-ipv4-${action}-${source}-to-${dest}":
-            order   => $priority,
+            order   => $order,
             target  => '/etc/shorewall/policy',
             content => "${source} ${dest} ${action} ${log_level}\n",
         }
@@ -27,7 +21,7 @@ define shorewall::policy (
 
     if $ipv6 {
         concat::fragment { "policy-ipv6-${action}-${source}-to-${dest}":
-            order   => $priority,
+            order   => $order,
             target  => '/etc/shorewall6/policy',
             content => "${source} ${dest} ${action} ${log_level}\n",
         }
